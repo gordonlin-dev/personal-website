f220a29c25b6359d8db67f39e9e9a14db0bb3d85
Gordon Lin <gordonlin.dev@gmail.com>
2023-01-10 16:33:04 -0500

    Added color themes to project (#13)
    * removed unused files
    
    * added tailwindcss to project
    
    * added main color themes to tailwind config

---
+++ src/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'background-main' : "#1C1C1E",
+      'color-text-header-dark-yellow' : "#F6AA1C",
+      'color-text-header-light-blue' : "#A6C2C9",
+      'color-text-body-white': "#FFFFFF"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
