d1029c1863ddc21fde5a16a307911ce446f580b9
Gordon Lin <gordonlin.dev@gmail.com>
2023-01-11 18:10:11 -0500

    finished Sticky menu (#14)
    * added grid layout
    
    * added top menu section
    
    * made top nav bar sticky
    
    * added positioning for sticky side table of content
    
    * implemented table of content
    
    * fixed link highlighting issue when scrolling

--- src/components/personal_website/mainLayout.js
+++ src/components/personal_website/mainLayout.js
@@ -1,8 +1,24 @@
+import TopNavigationMenu from "./topNavigationMenu";
+import TableOfContent from "./TableOfContent";
 export default function MainLayout({children}){
     return(
         <>
-            <main className="bg-background-main h-screen w-screen">
-                {children}
+            <main className="h-full w-full relative">
+                <div className="h-16 fixed top-0 left-0 right-0">
+                    <TopNavigationMenu/>
+                </div>
+                <div className="mt-16 grid grid-cols-12 bg-background-main">
+                    <div className="col-span-1">
+                    </div>
+                    <div className="col-span-9">
+                        {children}
+                    </div>
+                    <div className="col-span-2">
+                        <div className="flex w-full align-center justify-center h-full fixed top-[20%] left-[43%]">
+                            <TableOfContent/>
+                        </div>
+                    </div>
+                </div>
             </main>
         </>
     )
