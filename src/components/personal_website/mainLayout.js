ed41cb976b2453b95eb4638ec14c76d5ad1c0844
Gordon <gordonlin.dev@gmail.com>
2023-01-10 20:06:16 -0500

    added grid layout

--- src/components/personal_website/mainLayout.js
+++ src/components/personal_website/mainLayout.js
@@ -1,8 +1,24 @@
 export default function MainLayout({children}){
     return(
         <>
-            <main className="bg-background-main h-screen w-screen">
-                {children}
+            <main className="bg-background-main h-full w-full overflow-hidden">
+                <div className="grid grid-rows-7" >
+                    <div className="row-span-1 bg-background-header">
+                        <div>
+                            Test
+                        </div>
+                    </div>
+                    <div className="row-span-6">
+                        <div className="grid grid-cols-7">
+                            <div className="col-span-6">
+                                {children}
+                            </div>
+                            <div className="col-span-1">
+                                <div>test</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </main>
         </>
     )
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
   theme: {
     colors:{
       'background-main' : "#1C1C1E",
+      'background-header' : "#373743",
       'color-text-header-dark-yellow' : "#F6AA1C",
       'color-text-header-light-blue' : "#A6C2C9",
       'color-text-body-white': "#FFFFFF"
