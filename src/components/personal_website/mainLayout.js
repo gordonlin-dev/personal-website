f41a0024b6b7a09b7561de7517ba2f904ae9118c
Gordon <gordonlin.dev@gmail.com>
2023-01-10 22:00:11 -0500

    made top nav bar sticky

--- src/components/personal_website/mainLayout.js
+++ src/components/personal_website/mainLayout.js
@@ -2,28 +2,13 @@ import TopNavigationMenu from "./topNavigationMenu";
 export default function MainLayout({children}){
     return(
         <>
-            <main className="bg-background-main h-screen w-screen overflow-hidden">
-                <div className="h-16">
+            <main className="bg-background-main h-screen w-screen overflow-auto relative">
+                <div className="h-16 fixed top-0 left-0 right-0">
                     <TopNavigationMenu/>
                 </div>
-                <div>
+                <div className="mt-16">
                     {children}
                 </div>
-                {/*<div className="grid grid-rows-10" >
-                    <div className="row-span-3 bg-background-header">
-                        <TopNavigationMenu/>
-                    </div>
-                    <div className="row-span-7">
-                        <div className="grid-cols-7">
-                            <div className="col-span-6">
-                                {children}
-                            </div>
-                            <div className="col-span-1">
-                                <div>test</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>*/}
             </main>
         </>
     )
