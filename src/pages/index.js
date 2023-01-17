48193f4340455abde7a2f978b8b21a90c1d4b819
Gordon Lin <gordonlin.dev@gmail.com>
2023-01-17 01:25:58 -0500

    added landing page and made about me section more responsive (#43)

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,17 @@ import AboutMe from "../components/personal_website/content/aboutMe";
 import Experience from "../components/personal_website/content/experience";
 import Projects from "../components/personal_website/content/projects";
 import Contact from "../components/personal_website/content/contact";
+import Landing from "../components/personal_website/content/landing";
 
 
 export default function Index() {
   return (
-      <>
-        <MainLayout>
-            <div id="body" className="pt-20 pl-10 pr-10 pb-10">
-                <AboutMe/>
-                <Experience/>
-                <Projects/>
-                <Contact/>
-            </div>
-        </MainLayout>
-      </>
+      <MainLayout>
+          <Landing/>
+          <div className="pl-[12vw] pr-[12vw] lg:pt-[8vh]">
+              <AboutMe/>
+          </div>
+      </MainLayout>
   )
 }
 
